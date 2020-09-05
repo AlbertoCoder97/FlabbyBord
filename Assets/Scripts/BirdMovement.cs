d67fb5e2425a43c3745bbc345fd595b8829534bd
AlbertoCoder <alberto.calabrese.0297@gmail.com>
2020-09-05 18:57:38 +0200

    Added images + Basic Physics
    Added bird icon and object, background and colliders for when the bird touches the ground.

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+
+    public Collider2D birdCollider;
+    public Collider2D floorCollider;
+
+    public Vector2 speed;
+    public Vector2 gravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Vector2.down;
+        gravity = new Vector2(0, -150);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.down * -gravity);
+    }
+
+    void OnTriggerEnter2D(Collider2D floorCollider)
+    {
+        if(birdCollider.IsTouching(floorCollider))
+        {
+            Debug.Log("You lost!");
+            gravity = Vector2.zero;
+            speed = Vector2.zero;
+        }
+        
+    }
+}
