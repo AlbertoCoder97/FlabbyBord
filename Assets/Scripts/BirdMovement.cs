a285947531bed380e9d60d86bff12e21c8cd5dfb
AlbertoCoder <alberto.calabrese.0297@gmail.com>
2020-09-06 12:18:54 +0200

    Improved Physics + Bounds
    Improved Physics by changing from Transform operations to rigidBody phisics.
    Added maxHeight to avoid bird going out of the screen.

--- Assets/Scripts/BirdMovement.cs
+++ Assets/Scripts/BirdMovement.cs
@@ -1,37 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdMovement : MonoBehaviour
 {
 
+    //Max height
+    public int maxHeight = 5000;
+
     public Collider2D birdCollider;
     public Collider2D floorCollider;
 
-    public Vector2 speed;
-    public Vector2 gravity;
+    public Rigidbody2D birdBody;
+
+    //For gravity
+    public float speed;
+
+    //For jumping
+    public float force;
+    public Vector2 jump;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = Vector2.down;
-        gravity = new Vector2(0, -150);
+        //For jumping purposes
+        jump = Vector2.up;
+        force = 10000;
+
+        birdBody.gravityScale = 1000;
+
+        //Set initial velocity and then never change it
+        speed = 2500;
+        birdBody.velocity = Vector2.down * speed;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(Vector2.down * -gravity);
+        //Prevents bird to go out of bounds vertically
+        if (birdBody.position.y > maxHeight)
+            transform.position = new Vector2(transform.position.x, maxHeight);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("You jumped!");
+            birdBody.AddForce(jump * force, ForceMode2D.Impulse);
+        }
+            
     }
 
-    void OnTriggerEnter2D(Collider2D floorCollider)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(birdCollider.IsTouching(floorCollider))
         {
             Debug.Log("You lost!");
-            gravity = Vector2.zero;
-            speed = Vector2.zero;
+
+            //Just restart the game
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        
     }
+
+
 }
